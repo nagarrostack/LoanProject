48a2d47d893469643ec51945865573da5affc96b
Efrain <efrain.gracia@nagarro.com>
2022-08-12 15:39:53 -0500

    TypeCatalog & Catalog BL with tests.

--- Loans.web/Loans.BL/DependencyInjection.cs
+++ Loans.web/Loans.BL/DependencyInjection.cs
@@ -1,4 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using FluentValidation.AspNetCore;
+using Loans.BL.Client.Interfaces;
+using Loans.BL.Client.Services;
+using Loans.BL.Configuration.Interfaces;
+using Loans.BL.Configuration.Services;
+using Loans.BL.Loan.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Loans.BL
 {
@@ -6,6 +13,21 @@ namespace Loans.BL
     {
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
+            services
+                .AddTransient<ITypeCatalogService, TypeCatalogService>()
+                .AddTransient<ICatalogService, CatalogService>()
+                .AddTransient<IClientsService, ClientsService>()
+                //.AddTransient<IClientBusinessesService, IClientBusinessesService>()
+                //.AddTransient<IClientPhonesService, ClientPhonesService>()
+                ;
+
+            services.AddFluentValidation(options =>
+            {
+                options.RegisterValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
             return services;
         }
     }
---
+++ Loans.web/Loans.Testing/2_BusinessLogic/CatalogsBLTest.cs
@@ -0,0 +1,85 @@
+﻿using Loans.BL.Configuration.Interfaces;
+
+namespace Loans.Testing._2_BusinessLogic
+{
+    [TestClass]
+    public class CatalogsBLTest : BaseTest
+    {
+        [TestMethod]
+        public async Task GetAllCatalogs_TestAsync()
+        {
+            var service = serviceProvider.GetRequiredService<ICatalogService>();
+            var result = await service.GetAllCatalogsAsync();
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(context.Catalogs.Count(), result.Count);
+        }
+
+        [TestMethod,
+            DataTestMethod,
+            DataRow("phone"),
+            DataRow("Female"),
+            DataRow("India"),
+            DataRow("Mr"),
+            DataRow("Other")]
+        public async Task GetCatalogsByName_TestAsync(string name)
+        {
+            var service = serviceProvider.GetRequiredService<ICatalogService>();
+            var result = await service.GetCatalogsByNameAsync(name);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(context.Catalogs.Where(c => c.Name.ToUpper().Contains(name.ToUpper())).Count(), result.Count);
+        }
+
+        [TestMethod,
+            DataTestMethod,
+            DataRow(1),
+            DataRow(5),
+            DataRow(9)]
+        public async Task GetCatalogById_TestAsync(int id)
+        {
+            var service = serviceProvider.GetRequiredService<ICatalogService>();
+            var result = await service.GetCatalogByIdAsync(id);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(context.Catalogs.First(c => c.Id == id).Name, result.Name);
+        }
+
+        [TestMethod,
+            DataTestMethod,
+            DataRow(19),
+            DataRow(-5)]
+        public async Task GetCatalogById_OutOfRange_TestAsync(int id)
+        {
+            var service = serviceProvider.GetRequiredService<ICatalogService>();
+            var result = await service.GetCatalogByIdAsync(id);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod,
+            DataTestMethod,
+            DataRow(1),
+            DataRow(4)]
+        public async Task GetCatalogsByTypeCatalogId_TestAsync(int typeId)
+        {
+            var service = serviceProvider.GetRequiredService<ICatalogService>();
+            var result = await service.GetCatalogsByTypeCatalogIdAsync(typeId);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(context.Catalogs.Count(c => c.TypeCatalogId == typeId), result.Count);
+        }
+
+        [TestMethod,
+            DataTestMethod,
+            DataRow(8)]
+        public async Task GetCatalogsByTypeCatalogId_OutOfRange_TestAsync(int typeId)
+        {
+            var service = serviceProvider.GetRequiredService<ICatalogService>();
+            var result = await service.GetCatalogsByTypeCatalogIdAsync(typeId);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(context.Catalogs.Count(c => c.TypeCatalogId == typeId), result.Count);
+        }
+    }
+}
