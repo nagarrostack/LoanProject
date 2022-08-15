4eacf355a2517d60fe3f48cb0d12a635a6256341
Efrain <efrain.gracia@nagarro.com>
2022-08-15 16:09:35 -0500

    Client controller

--- Loans.web/Loans.web/Controllers/ClientController.cs
+++ Loans.web/Loans.web/Controllers/ClientController.cs
@@ -1,3 +1,4 @@
+using Loans.BL.Client.Interfaces;
 using Loans.web.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,16 +10,183 @@ namespace Loans.web.Controllers
     {
 
         private readonly ILogger<LoanController> _logger;
+        private readonly IClientsService service;
 
-        public ClientController(ILogger<LoanController> logger)
+        public ClientController(ILogger<LoanController> logger, IClientsService clientService)
         {
             _logger = logger;
+            service = clientService;
         }
 
         [HttpGet]
-        public IEnumerable<Client> Get()
+        public async Task<IEnumerable<Client>> Get()
         {
-            return null;
+            var response = await service.GetAllClientsAsync();
+
+            return response.Select(c => new Client
+            {
+                Id = c.Id,
+                CountryId = c.CountryId,
+                Country = c.CountryCatalog.Name,
+                GenderId = c.GenderId,
+                Gender = c.GenderCatalog.Name,
+                TitleId = c.TitleId,
+                Title = c.TitleCatalog.Name,
+                LastName = c.LastName,
+                MidName = c.MidName,
+                Name = c.Name
+            }).ToList();
+        }
+
+        [HttpGet("/ById/{id}")]
+        public async Task<Client> GetById(int id)
+        {
+            var response = await service.GetClientByIdAsync(id);
+
+            return new Client
+            {
+                Id = response.Id,
+                CountryId = response.CountryId,
+                Country = response.CountryCatalog.Name,
+                GenderId = response.GenderId,
+                Gender = response.GenderCatalog.Name,
+                TitleId = response.TitleId,
+                Title = response.TitleCatalog.Name,
+                LastName = response.LastName,
+                MidName = response.MidName,
+                Name = response.Name
+            };
+        }
+
+        [HttpGet("/ByName/{name}")]
+        public async Task<IEnumerable<Client>> GetByName(string name)
+        {
+            var response = await service.GetClientByNameAsync(name);
+
+            return response.Select(c => new Client
+            {
+                Id = c.Id,
+                CountryId = c.CountryId,
+                Country = c.CountryCatalog.Name,
+                GenderId = c.GenderId,
+                Gender = c.GenderCatalog.Name,
+                TitleId = c.TitleId,
+                Title = c.TitleCatalog.Name,
+                LastName = c.LastName,
+                MidName = c.MidName,
+                Name = c.Name
+            }).ToList();
+        }
+
+        [HttpGet("/ByGender/{idGender}")]
+        public async Task<IEnumerable<Client>> GetByGender(int idGender)
+        {
+            var response = await service.GetClientByGenderIdAsync(idGender);
+
+            return response.Select(c => new Client
+            {
+                Id = c.Id,
+                CountryId = c.CountryId,
+                Country = c.CountryCatalog.Name,
+                GenderId = c.GenderId,
+                Gender = c.GenderCatalog.Name,
+                TitleId = c.TitleId,
+                Title = c.TitleCatalog.Name,
+                LastName = c.LastName,
+                MidName = c.MidName,
+                Name = c.Name
+            }).ToList();
+        }
+
+        [HttpGet("/ByCountry/{idCountry}")]
+        public async Task<IEnumerable<Client>> GetByCountry(int idCountry)
+        {
+            var response = await service.GetClientByCountryIdAsync(idCountry);
+
+            return response.Select(c => new Client
+            {
+                Id = c.Id,
+                CountryId = c.CountryId,
+                Country = c.CountryCatalog.Name,
+                GenderId = c.GenderId,
+                Gender = c.GenderCatalog.Name,
+                TitleId = c.TitleId,
+                Title = c.TitleCatalog.Name,
+                LastName = c.LastName,
+                MidName = c.MidName,
+                Name = c.Name
+            }).ToList();
+        }
+
+        [HttpPost]
+        public async Task<Client> Post([FromBody] Client client)
+        {
+            var response = await service.SaveClientAsync(new BL.Client.Dtos.ClientDto
+            {
+                GenderId = client.GenderId,
+                CountryId = client.CountryId,
+                TitleId = client.TitleId,
+                Id = 0,
+                LastName = client.LastName,
+                MidName = client.MidName,
+                Name = client.Name
+
+            });
+
+            return new Client
+            {
+                Id = response.Id,
+                CountryId = response.CountryId,
+                Country = response.CountryCatalog.Name,
+                GenderId = response.GenderId,
+                Gender = response.GenderCatalog.Name,
+                TitleId = response.TitleId,
+                Title = response.TitleCatalog.Name,
+                LastName = response.LastName,
+                MidName = response.MidName,
+                Name = response.Name
+            };
+        }
+
+        [HttpPut("/{id}")]
+        public async Task<Client> Put(int id, [FromBody] Client client)
+        {
+            if (id == client.Id)
+            {
+                var response = await service.SaveClientAsync(new BL.Client.Dtos.ClientDto
+                {
+                    Id = client.Id,
+                    LastName = client.LastName,
+                    MidName = client.MidName,
+                    Name = client.Name,
+                    GenderId = client.GenderId,
+                    CountryId = client.CountryId,
+                    TitleId = client.TitleId
+                });
+
+                return new Client
+                {
+                    Id = response.Id,
+                    CountryId = response.CountryId,
+                    Country = response.CountryCatalog.Name,
+                    GenderId = response.GenderId,
+                    Gender = response.GenderCatalog.Name,
+                    TitleId = response.TitleId,
+                    Title = response.TitleCatalog.Name,
+                    LastName = response.LastName,
+                    MidName = response.MidName,
+                    Name = response.Name
+                };
+            }
+            throw new ArgumentException("Client's data doesn't match.");
+        }
+
+        [HttpDelete]
+        public async Task<bool> Del(int id)
+        {
+            var response = await service.DeleteClientAsync(id);
+
+            return response;
         }
     }
 }
