023118aefa698d514a24a050d41741cada665e45
Efrain <efrain.gracia@nagarro.com>
2022-08-15 16:08:11 -0500

    Configuration controller

---
+++ Loans.web/Loans.web/Controllers/ConfigurationController.cs
@@ -0,0 +1,46 @@
+using Loans.BL.Configuration.Interfaces;
+using Loans.web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Loans.web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConfigurationController : ControllerBase
+    {
+        private readonly ILogger<LoanController> _logger;
+        private readonly ICatalogService service;
+
+        public ConfigurationController(ILogger<LoanController> logger, ICatalogService catalogService)
+        {
+            _logger = logger;
+            service = catalogService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Catalog>> GetAll()
+        {
+            var response = await service.GetAllCatalogsAsync();
+
+            return response.Select(c => new Catalog
+            {
+                Id = c.Id,
+                TypeCatalogId = c.TypeCatalogId,
+                Name = c.Name
+            }).ToList();
+        }
+
+        [HttpGet("/ByType/{id}")]
+        public async Task<IEnumerable<Catalog>> GetByTypeId(int id)
+        {
+            var response = await service.GetCatalogsByTypeCatalogIdAsync(id);
+
+            return response.Select(c => new Catalog
+            {
+                Id = c.Id,
+                TypeCatalogId = c.TypeCatalogId,
+                Name = c.Name
+            }).ToList();
+        }
+    }
+}
