dac4352865acfbf5730b45359fd453a893cf82cf
Efrain <efrain.gracia@nagarro.com>
2022-08-16 08:12:57 -0500

    Client-side validations

--- Loans.web/Loans.WebApp/Models/Catalog.cs
+++ Loans.web/Loans.WebApp/Models/Catalog.cs
@@ -1,9 +1,16 @@
-﻿namespace Loans.WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Loans.WebApp.Models
 {
     public class Catalog
     {
         public int Id { get; set; }
+        
+        [Required]
         public int TypeCatalogId { get; set; }
+        
+        [Required,
+            StringLength(200)]
         public string Name { get; set; }
     }
 }
--- Loans.web/Loans.WebApp/Models/ClientBusiness.cs
+++ Loans.web/Loans.WebApp/Models/ClientBusiness.cs
@@ -1,12 +1,28 @@
-﻿namespace Loans.WebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Loans.WebApp.Models
 {
     public class ClientBusiness
     {
         public int Id { get; set; }
         public int ClientId { get; set; }
+        
+        [Required,
+            StringLength(200, MinimumLength = 10)]
         public string Address { get; set; }
+        
+        [Required,
+            StringLength(50)]
         public string Name { get; set; }
+        
+        [Required,
+            DataType(DataType.PhoneNumber),
+            StringLength(20),
+            Display(Name = "Phone number")]
         public string PhoneNumber { get; set; }
+        
+        [Required,
+            Display(Name = "Tax")]
         public int TaxId { get; set; }
         public Client Client { get; set; }
     }
